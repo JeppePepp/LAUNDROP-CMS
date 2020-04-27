87d6aa35187bc15d62344c8848617dea7ea4e6b1
Mirza Sumaiya <mirza01.alam@gmail.com>
2020-04-27 18:44:21 +0200

    committed

--- src/pages/Custom.vue
+++ src/pages/Custom.vue
@@ -3,7 +3,7 @@
         <div class="text-3xl font-semibold flex items-center justify-center">
             <Topic :topic='topic'/>
         </div>
-        <div class="paragraph text-sm">
+        <div class="px-64 ml-64 text-sm">
             Posted by Laundrop <br> Apil 22, 2020 / Stockholm <br>4 min read
         </div>
         
